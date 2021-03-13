7ce1257f5ad84b264a2efe100e487d9237c48be1
aruzhanabdikarim <55019066+aruzhanabdikarim@users.noreply.github.com>
2021-03-13 16:16:46 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+console.log("Its working")
+var http= require("http")
+var fs=require("fs")
+function serveStaticFile(res,path,contentType,responseCode){
+	if(!responseCode) responseCode=200;
+	fs.readFile(__dirname + path, function(err,data){
+		if (err){
+			res.writeHead(500,{"Content-Type" : "text/plain"})
+			res.end("500- Internal error")
+		}
+		else {
+			res.writeHead(responseCode,({"Content-Type" :contentType}));
+			res.end(data);
+		}
+		
+	})
+}
+http.createServer(function(req,res){
+	var path=req.url.replace(/\/?(?:\?.*)?$/,"").toLowerCase();
+	switch(path){
+		case "":
+		serveStaticFile(res,"/index.html","text/html");
+			break;
+		case "/about":
+		serveStaticFile(res,"/about.html","text/html");
+			break;
+		case "/img/about.jpg":
+		serveStaticFile(res,"/img/about.jpg","image/jpeg");
+			break;
+		case "/img/cry.jpg":
+		serveStaticFile(res,"/img/cry.jpg","image/jpeg");
+			break;
+		case "/img/welcome.jpg":
+		serveStaticFile(res,"/img/welcome.jpg","image/jpeg");
+			break;
+		case "/img/gallery/graduation.jpg":
+		serveStaticFile(res,"/img/gallery/graduation.jpg","image/jpeg");
+			break;
+		case "/img/gallery/study.jpg":
+		serveStaticFile(res,"/img/gallery/study.jpg","image/jpeg");
+			break;
+		case "/style.css":
+		serveStaticFile(res,"/style.css","text/css");
+			break;
+		case "/video/students/memes.mp4":
+		serveStaticFile(res,"/video/students/memes.mp4","video/mp4");
+			break;
+		default:
+		serveStaticFile(res,"/404.html","text/html",404);
+		break;
+	}	
+	}).listen(3000)
+	console.log("Serving is running on port 3000. Press CTRL + C to terminate");
